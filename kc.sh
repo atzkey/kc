292710a23be1670bdbbdd64f06200580c43f715f
Denis Bakunovitch <bakunovitch@gmail.com>
2011-08-17 01:19:06 +0300

    1am

--- kc.sh
+++ kc.sh
@@ -1,25 +1,19 @@
+set -e
 
-:<<KACHING
-account direction amount currency subject
-------------------------------------------
-cash    +         100    USD      salary
-cash    -         5      USD      food
-cash    -         6      USD      food
-cash    -         7      USD      food
-cash    -         15     USD      fuel
-cash    -         10     USD      exchange
-cash    +         500    BRB      exchange
-cash    -         500    BRB      food
+script=${0##*/}
+transaction=${script#kc}
+bank="$HOME/.kc"
+file=${bank}/`date '+%Y-%m-%d'`
 
-kc calculates number of transactions and remainder by currency, by currency and account, by currency and subject
-KACHING
+[ ! -d "$bank" ] && mkdir "$bank"
+touch "$file"
 
-program=`basename $0`
-
-if [ -z "$1" -o ! -r "$1" -o "$1" = "-h" -o "$1" = "--help" ] # Help! Help! Help!
+if [ -z "$1" -o "$1" = "-h" -o "$1" = "--help" ] # Help! Help! Help!
 then
   cat <<DOCUMENTATION
-Usage: $program <ka-ching file>
+Usage: kc [file ...]
+       kc+ <account> <amount> <currency> <category>
+       kc- <account> <amount> <currency> <category>
 DOCUMENTATION
   exit 0
 fi
@@ -30,33 +24,27 @@ awk '
     count[""] = 0;
     balance[""] = 0;
   }
-  {
-    # validate input
-    if (NF != 5) {
-      # do nothing
-    } else {
-      # assign field names
-      account = $1;
-      direction = $2;
-      amount = $3;
-      currency = $4;
-      subject = $5;
-      
-      # I"m the operator with my pocket calculator
-      count["* * * " currency " *"]++;
-      count["* * * " currency " " subject]++;
-      count[account " * * " currency " *"]++;
-      
-      # I am adding and subtracting
-      if (direction == "-") {
-        amount *= -1;
-      }
-      
-      # I"m controlling and composing
-      balance["* * * " currency " *"] += amount;
-      balance["* * * " currency " " subject] += amount;
-      balance[account " * * " currency " *"]+= amount;
+  NF == 5 {
+    account = $1;
+    direction = $2;
+    amount = $3;
+    currency = $4;
+    subject = $5;
+    
+    # I"m the operator with my pocket calculator
+    count["* " currency " balance"]++;
+    count[currency " " subject]++;
+    count[account " " currency " balance"]++;
+    
+    # I am adding and subtracting
+    if (direction == "-") {
+      amount *= -1;
     }
+    
+    # I"m controlling and composing
+    balance["* " currency " balance"] += amount;
+    balance[currency " " subject] += amount;
+    balance[account " " currency " balance"]+= amount;
   }
   END {
     # By pressing down a special key, it plays a little melody
